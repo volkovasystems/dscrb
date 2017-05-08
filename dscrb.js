8c6deb0c93275a6d7527bbd41b69e3971d28a3d9
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-08 16:03:39 +0800

    Improved code.

--- dscrb.js
+++ dscrb.js
@@ -34,6 +34,9 @@
 			"file": "dscrb.js",
 			"module": "dscrb",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/dscrb.git",
 			"test": "dscrb-test.js",
